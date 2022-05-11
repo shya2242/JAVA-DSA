1f8ea13bfff49ae62621aaadcd7b229b7a5fac95
shya2242 <102019283+shya2242@users.noreply.github.com>
2022-05-10 21:29:44 -0700

    Add files via upload
    Radius of Circle

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
+package com.Shyam;
+
+// Calculating the Area of circle
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+        System.out.print("Enter radius of Circle");
+        Scanner sn = new Scanner(System.in);
+        Double radius = sn.nextDouble();
+        Double area = Math.PI * radius * radius;
+        System.out.println("Area ="+area);
+
+        }
 }
